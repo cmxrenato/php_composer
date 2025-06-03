9ecf1105ec9ee64ac0e93d2c2119b067202f07fc
cmxrenato <renatoaraujoleal@gmail.com>
2025-06-03 15:25:04 -0300

    criação do construtor

--- src/ContaBancaria.php
+++ src/ContaBancaria.php
@@ -22,6 +22,44 @@ class ContaBancaria
         ];
 
 }
+    public function __construct(
+        string $banco,
+        string $nomeTitular,
+        string $numeroAgencia,
+        string $numeroConta,
+        float $saldo
+        )
+    {
+        $this->banco = $banco;
+        $this->nomeTitular = $nomeTitular;
+        $this->numeroAgencia = $numeroAgencia;
+        $this->numeroConta = $numeroConta;
+        $this->saldo = $saldo;
+
+
+
+        }
+
+
+     public function depositar(float $valor):string
+     {
+         $this->saldo += $valor;
+         return "Depósito de R$".number_format($valor,2,',','')." realizado";
+         }
+
+     public function sacar(float $valor):string
+     {
+         $this->saldo -= $valor;
+         return "Saque de R$".number_format($valor,2,',','')." realizado";
+         }
+
+     public function obterSaldo():string
+     {
+           return "Saldo em R$: ".number_format($this->saldo,2,',','');
+         }
+
+
+
     
      public function setBanco(string $novoBanco):void
      {
