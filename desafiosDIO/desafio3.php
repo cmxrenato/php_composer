a4bae5548ca866b1f652ff84081b7082deb689b6
cmxrenato <renatoaraujoleal@gmail.com>
2025-06-10 12:32:22 -0300

    realização do desafio3 finalizada

--- desafiosDIO/desafio3.php
+++ desafiosDIO/desafio3.php
@@ -1,24 +1,25 @@
+
 <?php
 $valor = (float) fgets(STDIN);
 //$valor = 576.73;
-
+$valor_convertido = $valor * 100;
 $notas_ideal = array(100.00, 50.00, 20.00, 10.00, 5.00, 2.00);
 $notas = array();
 $moedas_ideal = array(1.00, 0.50, 0.25, 0.10, 0.05, 0.01);
 $moedas = array();
 
-$notas[0] = intdiv($valor, 100);
-$notas[1] = intdiv(($valor - $notas[0] * 100), 50);
-$notas[2] = intdiv(($valor - $notas[1] * 50 - $notas[0] * 100), 20);
-$notas[3] = intdiv(($valor - $notas[2] * 20 - $notas[1] * 50 - $notas[0] * 100), 10);
-$notas[4] = intdiv(($valor - $notas[3] * 10 - $notas[2] * 20 - $notas[1] * 50 - $notas[0] * 100), 5);
-$notas[5] = intdiv(($valor - $notas[4] * 5 - $notas[3] * 10 - $notas[2] * 20 - $notas[1] * 50 - $notas[0] * 100), 2);
-$moedas[0] = intdiv(($valor - $notas[4] * 5 - $notas[3] * 10 - $notas[2] * 20 - $notas[1] * 50 - $notas[0] * 100), 1);
-$moedas[1] = intdiv((($valor - $notas[4] * 5 - $notas[3] * 10 - $notas[2] * 20 - $notas[1] * 50 - $notas[0] * 100 - $moedas[0] * 1) * 100), 50);
-$moedas[2] = intdiv((($valor - $notas[4] * 5 - $notas[3] * 10 - $notas[2] * 20 - $notas[1] * 50 - $notas[0] * 100 - $moedas[0] * 1 - $moedas[1] * 0.5) * 100), 25);
-$moedas[3] = intdiv((($valor - $notas[4] * 5 - $notas[3] * 10 - $notas[2] * 20 - $notas[1] * 50 - $notas[0] * 100 - $moedas[0] * 1 - $moedas[1] * 0.5 - $moedas[2] * 0.25) * 100), 10);
-$moedas[4] = intdiv((($valor - $notas[4] * 5 - $notas[3] * 10 - $notas[2] * 20 - $notas[1] * 50 - $notas[0] * 100  - $moedas[0] * 1 - $moedas[1] * 0.5 - $moedas[2] * 0.25 - $moedas[3] * 0.10) * 100), 5);
-$moedas[5] = intdiv((($valor - $notas[4] * 5 - $notas[3] * 10 - $notas[2] * 20 - $notas[1] * 50 - $notas[0] * 100  - $moedas[0] * 1 - $moedas[1] * 0.5 - $moedas[2] * 0.25 - $moedas[3] * 0.10 - $moedas[4] * 0.05) * 100), 1);
+$notas[0] = intdiv($valor_convertido, 10000);
+$notas[1] = intdiv(($valor_convertido - $notas[0] * 10000), 5000);
+$notas[2] = intdiv(($valor_convertido - $notas[0] * 10000 - $notas[1] * 5000), 2000);
+$notas[3] = intdiv(($valor_convertido - $notas[0] * 10000 - $notas[1] * 5000 - $notas[2] * 2000), 1000);
+$notas[4] = intdiv(($valor_convertido - $notas[0] * 10000 - $notas[1] * 5000 - $notas[2] * 2000 - $notas[3] * 1000), 500);
+$notas[5] = intdiv(($valor_convertido - $notas[0] * 10000 - $notas[1] * 5000 - $notas[2] * 2000 - $notas[3] * 1000 - $notas[4] * 500), 200);
+$moedas[0] = intdiv(($valor_convertido - $notas[0] * 10000 - $notas[1] * 5000 - $notas[2] * 2000 - $notas[3] * 1000 - $notas[4] * 500 - $notas[5] * 200), 100);
+$moedas[1] = intdiv((($valor_convertido - $notas[0] * 10000 - $notas[1] * 5000 - $notas[2] * 2000 - $notas[3] * 1000 - $notas[4] * 500 - $notas[5] * 200 - $moedas[0] * 100)), 50);
+$moedas[2] = intdiv((($valor_convertido - $notas[0] * 10000 - $notas[1] * 5000 - $notas[2] * 2000 - $notas[3] * 1000 - $notas[4] * 500 - $notas[5] * 200 - $moedas[0] * 100 - $moedas[1] * 50)), 25);
+$moedas[3] = intdiv((($valor_convertido - $notas[0] * 10000 - $notas[1] * 5000 - $notas[2] * 2000 - $notas[3] * 1000 - $notas[4] * 500 - $notas[5] * 200 - $moedas[0] * 100 - $moedas[1] * 50 - $moedas[2] * 25)), 10);
+$moedas[4] = intdiv((($valor_convertido - $notas[0] * 10000 - $notas[1] * 5000 - $notas[2] * 2000 - $notas[3] * 1000 - $notas[4] * 500 - $notas[5] * 200  - $moedas[0] * 100 - $moedas[1] * 50 - $moedas[2] * 25 - $moedas[3] * 10)), 5);
+$moedas[5] = intdiv((($valor_convertido - $notas[0] * 10000 - $notas[1] * 5000 - $notas[2] * 2000 - $notas[3] * 1000 - $notas[4] * 500 - $notas[5] * 200  - $moedas[0] * 100 - $moedas[1] * 50 - $moedas[2] * 25 - $moedas[3] * 10 - $moedas[4] * 5)), 1);
 
 
 echo "NOTAS:";
